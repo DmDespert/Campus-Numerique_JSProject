cfc6ccacb617aec4b2678979b7b1daad8e7a04ca
DmDespert <ordimac@jim-dede.(none)>
2021-03-25 13:42:56 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--FAVICO-->
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    <!--TITLE & DESCRPTION-->
+    <title><?php echo $pagesMetaTitles[$url] ?></title>
+    <meta name="description" content="<?php echo $pagesMetaDescriptions[$url] ?>"/>
+    <!--CSS-->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <!--FONTS-->
+    <link href="https://fonts.googleapis.com/css2?family=Bungee&family=Cutive+Mono&family=Special+Elite&display=swap" rel="stylesheet">
+    <!--CSS ANIMATIONS-->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+</head>
+<body>
+    <header id="top">
+        <nav>
+            <div class="logo bungee">ROBCAD.com</div>
+            <ul>
+                <li><a href="#top">Home</a></li>
+                <li><a href=gallery.php">Gallery</a></li>
+                <li><a href="game.php">Game</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
+        <div class="container">
+            <div class="row">
+                <h1 class="bungee">Welcome to Robcad</h1>
+            </div>
+            <div class="row">
+                <h2 class="second-title special">Bored ? Let's play a <a href="" class="mcolor">game</a></h2>
+            </div>
+        </div>
+
+    </header>
+    <main>
+        <section>
+            <div class="container">
+                <div class="row">
+                    <h3>Last posts</h3>
+                </div>
+                <div class="row">
+
+                </div>
+            </div>
+        </section>
+        <section>
+
+        </section>
+    </main>
+    <footer>
+        <div class="container">
+            <div class="row"></div>
+        </div>
+    </footer>
+</body>
+</html>
